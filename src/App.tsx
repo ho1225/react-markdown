f92d6bd7336157f87bcf9df54b51f37089db3a25
MSI\hosiu <hosiuchuen05@gmail.com>
2023-10-05 00:36:58 -0400

    finish create and add noteform into localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
 import { Routes, Route, Navigate } from "react-router-dom";
-import { NewNote } from "./NewNote";
+import { NewNote } from "./components/NewNote";
 import { useLocalStorage } from "./hook/useLocalStorage";
+import { useMemo } from "react";
+import { v4 as uuidV4 } from "uuid";
+import { NoteList } from "./components/NoteList";
 
 export type Note = {
   id: string;
@@ -32,11 +35,47 @@ export type Tag = {
 function App() {
   const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", []);
   const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", []);
+
+  const notesWithTags = useMemo(() => {
+    return notes.map((note) => {
+      return {
+        ...note,
+        tags: tags.filter((tag) => note.tagIds.includes(tag.id)),
+      };
+    });
+  }, [notes, tags]);
+
+  function onCreateNote({ tags, ...data }: NoteData) {
+    setNotes((prevNotes) => {
+      console.log([
+        ...prevNotes,
+        { ...data, id: uuidV4(), tagIds: tags.map((tag) => tag.id) },
+      ]);
+      return [
+        ...prevNotes,
+        { ...data, id: uuidV4(), tagIds: tags.map((tag) => tag.id) },
+      ];
+    });
+  }
+
+  function addTag(tag: Tag) {
+    setTags((prevTags) => [...prevTags, tag]);
+  }
+
   return (
     <Container className="my-4">
       <Routes>
-        <Route path="/" element={<h1>Home</h1>}></Route>
-        <Route path="/new" element={<NewNote />}></Route>
+        <Route path="/" element={<NoteList />}></Route>
+        <Route
+          path="/new"
+          element={
+            <NewNote
+              onSubmit={onCreateNote}
+              onAddTag={addTag}
+              availableTags={tags}
+            />
+          }
+        ></Route>
         <Route path="/:id">
           <Route index element={<h1>show</h1>}></Route>
           <Route path="edit" element={<h1>edit</h1>}></Route>
